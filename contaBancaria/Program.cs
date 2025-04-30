bba088cd4cb5ed8e7b0c92ae127386e118da6c5d
renatoads1 <renato.lacerda>
2025-04-30 15:19:17 -0300

    override e virtual

--- contaBancaria/Program.cs
+++ contaBancaria/Program.cs
@@ -1,28 +1,38 @@
 ﻿
 using contaBancaria.Entities;
-
+//parte 2
 Conta conta = new Conta("João", "1234", "56789-0");
-conta.Depositar(1000);
+Conta contaEmpresa = new ContaEmpresa("Maria", "1234", "56789-1", 5000);
+
 conta.Sacar(200);
-Console.WriteLine($"Titular: {conta.Titular}");
-Console.WriteLine($"Agência: {conta.Agencia}");
-Console.WriteLine($"Conta Corrente: {conta.ContaCorrente}");
-Console.WriteLine($"Saldo: {conta.Saldo}");
-ContaEmpresa contaEmpresa = new ContaEmpresa("Maria", "1234", "56789-1", 5000);
-contaEmpresa.Depositar(2000);
-contaEmpresa.Emprestimo(1000);
-ContaInvesteFacil contaInvesteFacil = new ContaInvesteFacil("Carlos", "1234", "56789-2", 10000);
+Console.WriteLine(conta.Saldo);
+contaEmpresa.Sacar(200);
+Console.WriteLine(contaEmpresa.Saldo);
 
 
-//upcasting
-//subclase para super classe
-Conta c1 = contaEmpresa;
-Conta c2 = contaInvesteFacil;
+//parte 1
+//Conta conta = new Conta("João", "1234", "56789-0");
+//conta.Depositar(1000);
+//conta.Sacar(200);
+//Console.WriteLine($"Titular: {conta.Titular}");
+//Console.WriteLine($"Agência: {conta.Agencia}");
+//Console.WriteLine($"Conta Corrente: {conta.ContaCorrente}");
+//Console.WriteLine($"Saldo: {conta.Saldo}");
+//ContaEmpresa contaEmpresa = new ContaEmpresa("Maria", "1234", "56789-1", 5000);
+//contaEmpresa.Depositar(2000);
+//contaEmpresa.Emprestimo(1000);
+//ContaInvesteFacil contaInvesteFacil = new ContaInvesteFacil("Carlos", "1234", "56789-2", 10000);
 
-//Downcasting
-//superclase para subclase
-ContaEmpresa c3 = new Conta() as ContaEmpresa;
-ContaEmpresa c4 = (ContaEmpresa)conta;
-if (c3 is ContaEmpresa) { 
-    c3 = new Conta( "1234", "56789-1","321321321") as ContaEmpresa;
-}
+
+////upcasting
+////subclase para super classe
+//Conta c1 = contaEmpresa;
+//Conta c2 = contaInvesteFacil;
+
+////Downcasting
+////superclase para subclase
+//ContaEmpresa c3 = new Conta() as ContaEmpresa;
+//ContaEmpresa c4 = (ContaEmpresa)conta;
+//if (c3 is ContaEmpresa) { 
+//    c3 = new Conta( "1234", "56789-1","321321321") as ContaEmpresa;
+//}
